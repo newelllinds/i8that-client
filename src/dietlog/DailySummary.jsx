8bd8015f2a3f29e875e8289c5190242cb98a6016
Exhale Yoga <exhaleyoga@exhales-air.lan>
2021-02-22 21:18:58 -0500

    Daily Calorie Summary

--- src/dietlog/DailySummary.jsx
+++ src/dietlog/DailySummary.jsx
@@ -1,65 +1,44 @@
-// import React from 'react';
-// import { Table, Button, Container, Row, Col, Alert } from 'reactstrap';
+import React, {useState} from 'react';
+import { Table, Button, Container, Row, Col, Alert } from 'reactstrap';
 
-// const DailySummary = (props) => {
-//         fetch(`http://localhost:3000/dietlog/...`, {
-//             method: 'DELETE',
-//             headers: new Headers({
-//                 'Content-Type': 'application/json',
-//                 'Authorization': props.token
-//             })
-//         }).then(() => props.fetchDietlogs())
-        
-//     }
+const DailySummary = (props) => {
+    const [totalCalories, setTotalCalories] = useState(0);
+    var total = 0
+    var today = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"})
+    today = new Date(today).toISOString().slice(0,10);
+    console.log(today)
+    let dateSelected = today
+    let id = 3
+        fetch(`http://localhost:3000/dietlog/${dateSelected}/${id}`, {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': props.token
+            })
 
-//     const dailySummaryMapper = () => {
-//         return props.dietlogs.map((dietlog, index) => {
-//             return (
-//                 <tr key={index}>
-//                     <th scope="row">{dietlog.id}</th>
-//                     <td>{dietlog.food_item}</td>
-//                     <td>{dietlog.calories}</td>
-//                     <td>{dietlog.date_eaten}</td>
-//                     <td>{dietlog.where_eaten}</td>
-//                     <td>{dietlog.feelings}</td>
-//                     <td>{dietlog.image}</td>
-//                     <td>
-//                         <Button color="secondary" onClick={() => { props.editUpdateDietlog(dietlog); props.updateOn() }}>Update Calorie Goal</Button>
-//                         {/* <Button color="danger" onClick={() => {deleteDietlog(dietlog)}}>Delete</Button> */}
-//                     </td>
+        })
+        .then((res) => res.json())
+        .then((json) => {
+            Object.entries(json).forEach(entry => {
+                total += entry[1].calories
+            })
+            setTotalCalories(total)
+        })     
 
+    return (
+        <div className="table">
+            <Container>
+                <Row>
+                    <Col>
                     
-//                 </tr>
-//             )
-//         })
-//     }
+            <h3>Calories Consumed Today</h3>
+            {totalCalories}
 
-//     return (
-//         <div className="table">
-//             <Container>
-//                 <Row>
-//                     <Col>
-                    
-//             <h3>Your Dietlog</h3>
-//             <hr />
-//             <Table striped hover>
-//                 <thead>
-//                     <tr>
-//                         <th>#</th>
-//                         <th>Calories Remaining</th>
-//                         <th>Calorie Goal</th>
-//                         <th>Calories Consumed</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     {dietlogMapper()}
-//                 </tbody>
-//                         </Table>
-//                     </Col>
-//                 </Row>
-//             </Container>
-//             </div>
-//     )
-// }
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
 
-// export default DailySummary;
+export default DailySummary;
